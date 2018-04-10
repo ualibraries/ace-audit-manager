38cd6b181185d9ca48f23cba7b334203ffd2e5fa
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-04-10 13:28:25 -0700

    Initial docker image creation works, am able to get ace audit coming up under http://localhost/ace-am

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM tomcat:8.5-jre8
+
+ENV ACE_AUDIT_MANAGER_V=1.11 ACE_AUDIT_TAR=ace-am-1.11-RELEASE MYSQL_JCONNECT_V=5.1.46 MYSQL_JCONNECT=mysql-connector-java-5.1.46
+
+RUN \
+cd /opt && \
+curl -kL https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-$MYSQL_JCONNECT_V.tar.gz | tar xz && \
+cp $MYSQL_JCONNECT/$MYSQL_JCONNECT-bin.jar /usr/local/tomcat/lib && \
+curl -kL https://obj.umiacs.umd.edu/ace/releases/$ACE_AUDIT_MANAGER_V/ace-dist-$ACE_AUDIT_MANAGER_V-RELEASE-bin.tar.gz | tar xz && \
+cp $ACE_AUDIT_TAR/ace-am.war /usr/local/tomcat/webapps/ && \
+mkdir -p /usr/local/tomcat/conf/Catalina/localhost && \
+cp $ACE_AUDIT_TAR/ace-am.xml /usr/local/tomcat/conf/Catalina/localhost && \
+mkdir /opt/initdb.d && \
+cp $ACE_AUDIT_TAR/ace-am.sql /opt/initdb.d && \
+sed -i -e "s|YOUR_PASSWORD|ace-audit-5231@&|g" /usr/local/tomcat/conf/Catalina/localhost/ace-am.xml && \
+sed -i -e "s|localhost/aceam|db-host:3306/aceamdb|g" /usr/local/tomcat/conf/Catalina/localhost/ace-am.xml && \
+sed -i -e "s|YOUR_PASSWORD|ace-audit-5231@&|g" /opt/initdb.d/ace-am.sql && \
+sed -i -e "s|MyISAM|InnoDB|g" /opt/initdb.d/ace-am.sql
+
+VOLUME ["/opt/initdb.d"]
---
+++ dbuild.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -x
+
+NETWORK=${5:-host}
+
+TAG=${4:-1.11}
+CONTAINER=${3:-ace-audit-fixity-test}
+REPOSITORY=${2:-ace-audit-manager}
+ACTION=${1:-BUILD}
+DAEMONIZE=-d
+
+# Delete test container built from docker file
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+if [ "$ACTION" = "BUILD" ]; then
+# Delete test image built from docker file
+docker image rm $REPOSITORY:$TAG
+
+# Create test image from docker file
+docker build --rm -t $REPOSITORY:$TAG .
+
+ACTION=DEBUG
+
+fi
+
+if [ "$ACTION" = "DEBUG" ]; then
+    DAEMONIZE=""
+    DEBUG="--user root -it --entrypoint /bin/bash"
+fi
+
+if [ "$ACTION" = "COMPOSE" ]; then
+
+NETWORK=test_default
+
+docker run --user root -it --entrypoint /bin/bash \
+       --net=$NETWORK \
+       --link db-host:test_db-host_1 \
+       -e DB_HOST="db-host" \
+       --tmpfs /var/log/php-fpm:uid=33,gid=33,mode=755,noexec,nodev,nosuid \
+       --tmpfs /run/lock:uid=0,gid=0,mode=1777,noexec,nodev \
+       --tmpfs /run/php:uid=33,gid=33,mode=775,noexec,nodev,nosuid \
+       --name $CONTAINER \
+       $REPOSITORY:$TAG
+
+else
+
+docker run $DAEMONIZE $DEBUG \
+       --net=$NETWORK \
+       --name $CONTAINER \
+       $REPOSITORY:$TAG
+
+fi
